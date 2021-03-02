1796ba6f09534041b773321a1985442d8553086e
Arturo Olmos <aolmos@brooktec.com>
2021-03-02 12:21:18 +0100

    part 2 tutorial: Display a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    
   ],
   imports: [
     BrowserModule,
